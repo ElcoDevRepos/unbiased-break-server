eac2775dc929d30881a46c91f7b792917ce7375a
Austin Hunter <ahunter8850@outlook.com>
2023-05-11 21:46:21 -0500

    Update get_related_job.js

--- get_related_job.js
+++ get_related_job.js
@@ -24,7 +24,7 @@ async function getRelatedArticles () {
     console.log("RUNNING...");
 
     const timestampFrame = new Date(Date.now() - 6 * 60 * 60 * 1000);     //Timestamp for the timeframe of reads
-    const minimumSimilarity = 0.35;                                       //Minimum value of similarity to determine if two articles are similar
+    const minimumSimilarity = 0.55;                                       //Minimum value of similarity to determine if two articles are similar
 
     //Get a query reference snapshot from the last 24h for left, middle and right articles
     const queryLeftArticles = await db.collection("left-articles").where("timestamp", ">=", timestampFrame).get();
