20a2306c84f73db97f0121272256260697b153b7
Austin Hunter <ahunter8850@outlook.com>
2023-10-20 15:25:48 -0500

    truncate txt

--- get_gpt_summaries_job.js
+++ get_gpt_summaries_job.js
@@ -1,94 +1,102 @@
 const admin = require("firebase-admin");
 const fetch = require("node-fetch");
-var cron = require('node-cron');
+var cron = require("node-cron");
 var serviceAccount = require("./serviceAccountKey.json");
-require('dotenv').config();
+require("dotenv").config();
 
-const openai = require('openai'); 
+const openai = require("openai");
 
 const api = new openai.OpenAI({
-    apiKey: process.env.MY_SECRET_KEY
+  apiKey: process.env.MY_SECRET_KEY,
 });
 
-const PORT = process.env.PORT || 8081
+const PORT = process.env.PORT || 8081;
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 const db = admin.firestore();
 
 async function getGPTSummaries() {
-    console.log("RUNNING GPT SUMMARIES...");
+  console.log("RUNNING GPT SUMMARIES...");
 
-    const timestampFrame = new Date(Date.now() - 24 * 60 * 60 * 1000); //Timestamp for the timeframe of reads
+  const timestampFrame = new Date(Date.now() - 24 * 60 * 60 * 1000); //Timestamp for the timeframe of reads
 
-    const queryTrendingArticles = await db.collection("trending-articles").where("timestamp", ">=", timestampFrame).get();
+  const queryTrendingArticles = await db
+    .collection("trending-articles")
+    .where("timestamp", ">=", timestampFrame)
+    .get();
 
-    // Local array to save the summaries
-    let summariesArray = [];
+  // Local array to save the summaries
+  let summariesArray = [];
 
-    const promises = queryTrendingArticles.docs.map(async (doc) => {
-        let txt = doc.data().textBody; //Grabs reference for the document text body
-        let timestamp = doc.data().date; //Grabs reference for the document timestamp
-        let source = doc.data().siteName; //Grabs reference for the document source
-        let title = doc.data().title; //Grabs reference for the document title
-        let link = doc.data().link; //Grabs reference for the document link
-        let id = doc.data().id; //Grabs reference for the document id
-        let image = null;
-        if(doc.data().image != null) image = doc.data().image; //Grabs reference for the document image if there is one
-        txt = removeDoubleSpaces(txt);
+  const promises = queryTrendingArticles.docs.map(async (doc) => {
+    let txt = doc.data().textBody; //Grabs reference for the document text body
+    let timestamp = doc.data().date; //Grabs reference for the document timestamp
+    let source = doc.data().siteName; //Grabs reference for the document source
+    let title = doc.data().title; //Grabs reference for the document title
+    let link = doc.data().link; //Grabs reference for the document link
+    let id = doc.data().id; //Grabs reference for the document id
+    let image = null;
+    if (doc.data().image != null) image = doc.data().image; //Grabs reference for the document image if there is one
+    txt = removeDoubleSpaces(txt);
 
-        //Prevent Washington Post articles from summary
-        if(source != 'The Washington Post') {
-            try {
-                const chatCompletion = await api.chat.completions.create({
-                    model: "gpt-3.5-turbo",
-                    messages: [{"role": "user", "content": generatePrompt(txt)}],
-                });
+    const MAX_TOKENS = 4090;
+    const ESTIMATED_CHAR_PER_TOKEN = 5; // This is a rough estimate
 
-                const response = chatCompletion.choices[0].message.content;
-
-                // Save the summary to the local array instead of firestore directly
-                summariesArray.push({
-                    summary: response,
-                    timestamp: timestamp,
-                    source: source,
-                    title: title,
-                    image: image,
-                    link: link,
-                    id: id
-                });
-
-            } catch (err) {
-                console.log(err);
-            }
-        }
-    });
-
-    await Promise.all(promises);
-
-    // Save the local array of summaries to one Firestore document
-    try {
-        await db.collection('gpt-summaries').add({
-            summaries: summariesArray,
-            timestamp: admin.firestore.FieldValue.serverTimestamp()
-        });
-    } catch (error) {
-        console.log("Error saving summaries to Firestore:", error);
+    if (txt.length > MAX_TOKENS * ESTIMATED_CHAR_PER_TOKEN) {
+      txt = txt.substring(0, MAX_TOKENS * ESTIMATED_CHAR_PER_TOKEN) + "..."; // Add ellipsis to indicate truncation
     }
+    //Prevent Washington Post articles from summary
+    if (source != "The Washington Post") {
+      try {
+        const chatCompletion = await api.chat.completions.create({
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content: generatePrompt(txt) }],
+        });
 
-    console.log("GPT SUMMARIES COMPLETE!")
+        const response = chatCompletion.choices[0].message.content;
+
+        // Save the summary to the local array instead of firestore directly
+        summariesArray.push({
+          summary: response,
+          timestamp: timestamp,
+          source: source,
+          title: title,
+          image: image,
+          link: link,
+          id: id,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  });
+
+  await Promise.all(promises);
+
+  // Save the local array of summaries to one Firestore document
+  try {
+    await db.collection("gpt-summaries").add({
+      summaries: summariesArray,
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    });
+  } catch (error) {
+    console.log("Error saving summaries to Firestore:", error);
+  }
+
+  console.log("GPT SUMMARIES COMPLETE!");
 }
 
 function generatePrompt(textBody) {
-    return `Create a two sentence summary for this news article: ${textBody}`;
+  return `Create a two sentence summary for this news article: ${textBody}`;
 }
 
 function removeDoubleSpaces(inputString) {
-    return inputString.replace(/\s{2,}/g, ' ');
+  return inputString.replace(/\s{2,}/g, " ");
 }
 
 function init() {
-    Promise.all([getGPTSummaries()]).then(() => process.exit());
+  Promise.all([getGPTSummaries()]).then(() => process.exit());
 }
 
 init();
